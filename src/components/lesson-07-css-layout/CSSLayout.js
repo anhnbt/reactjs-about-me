dd4cda38ede509e6ffa98b6604d629ddf3ed0623
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2024-10-12 16:46:30 +0700

    Update CSSLayout.js

--- src/components/lesson-07-css-layout/CSSLayout.js
+++ src/components/lesson-07-css-layout/CSSLayout.js
@@ -176,52 +176,176 @@ const CSSLayout = () => {
         <div className="py-10 px-4">
           <div className="max-w-7xl mx-auto" data-aos="fade-up">
             <Card hoverable className="shadow-lg">
-              <div className="grid grid-cols-1 md:grid-cols-[3fr_1fr] gap-8">
-                <div className="flex flex-col">
-                  <Title level={2} className="text-left">
-                    CSS Float Layout
-                  </Title>
-                  <p className="text-lg text-left">
-                    Thuộc tính float được sử dụng để định vị và định dạng nội
-                    dung, ví dụ: để một hình ảnh trôi sang trái của văn bản
-                    trong một vùng chứa.
-                  </p>
-                  <p className="text-lg text-left">
-                    Thuộc tính float có thể có một trong các giá trị sau:
-                  </p>
-                  <ul className="list-inside list-disc text-lg">
-                    <li>
-                      <code class="border border-gray-300 text-blue-600">
-                        left
-                      </code>{" "}
-                      – Phần tử nổi ở bên trái vùng chứa của nó
-                    </li>
-                    <li>
-                      <code class="border border-gray-300 text-blue-600">
-                        right
-                      </code>{" "}
-                      – Phần tử nổi ở bên phải vùng chứa của nó
-                    </li>
-                    <li>
-                      <code class="border border-gray-300 text-blue-600">
-                        none
-                      </code>{" "}
-                      – Phần tử không nổi (sẽ được hiển thị ngay tại nơi nó xuất
-                      hiện trong văn bản). Đây là mặc định
-                    </li>
-                    <li>
-                      <code class="border border-gray-300 text-blue-600">
-                        inherit
-                      </code>{" "}
-                      – Phần tử kế thừa giá trị float của phần tử cha của nó
-                    </li>
-                  </ul>
-                  <p className="text-lg">
-                    Trong cách sử dụng đơn giản nhất, thuộc tính float có thể
-                    được sử dụng để bọc văn bản xung quanh hình ảnh.
-                  </p>
-                </div>
-              </div>
+              <Title level={2} className="text-left">
+                Giới thiệu về CSS Float
+              </Title>
+              <p className="text-lg text-left">
+                Thuộc tính{" "}
+                <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                  float
+                </code>{" "}
+                được sử dụng để định vị các phần tử bên trái hoặc phải của
+                container.
+              </p>
+              <p className="text-lg text-left">
+                Ban đầu,{" "}
+                <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                  float
+                </code>{" "}
+                được thiết kế để sử dụng với văn bản (ví dụ: căn ảnh sang một
+                bên để văn bản bao quanh).
+              </p>
+              <p className="text-lg text-left">
+                Hiện tại,{" "}
+                <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                  float
+                </code>{" "}
+                thường được sử dụng trong các bố cục web đơn giản, nhưng không
+                còn là phương pháp layout chính do sự ra đời của Flexbox và
+                Grid.
+              </p>
+              <p className="text-lg text-left">
+                Thuộc tính{" "}
+                <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                  float
+                </code>{" "}
+                có thể có một trong các giá trị sau:
+              </p>
+              <ul className="list-inside list-disc text-lg">
+                <li>
+                  <code class="border border-gray-300 text-blue-600">left</code>{" "}
+                  – Phần tử nổi ở bên trái vùng chứa của nó
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">
+                    right
+                  </code>{" "}
+                  – Phần tử nổi ở bên phải vùng chứa của nó
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">none</code>{" "}
+                  – Phần tử không nổi (sẽ được hiển thị ngay tại nơi nó xuất
+                  hiện trong văn bản). Đây là mặc định
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">
+                    inherit
+                  </code>{" "}
+                  – Phần tử kế thừa giá trị float của phần tử cha của nó
+                </li>
+              </ul>
+              <p className="text-lg">
+                Trong cách sử dụng đơn giản nhất, thuộc tính float có thể được
+                sử dụng để bọc văn bản xung quanh hình ảnh.
+              </p>
+            </Card>
+          </div>
+        </div>
+      </section>
+      <section className="mb-12">
+        <div className="bg-white py-10 px-4">
+          <div className="max-w-7xl mx-auto" data-aos="fade-up">
+            <Card hoverable className="shadow-lg">
+              <Title level={2} className="text-left">
+                Khái niệm về Thuộc tính display
+              </Title>
+              <p className="text-lg">
+                <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                  display
+                </code>{" "}
+                xác định cách phần tử hiển thị trên trang
+              </p>
+              <p className="text-lg">Các giá trị phổ biến:</p>
+              <ul className="list-inside list-disc text-lg">
+                <li>
+                  <code class="border border-gray-300 text-blue-600">
+                    block
+                  </code>
+                  : Phần tử chiếm toàn bộ chiều rộng, bắt đầu từ một dòng mới
+                  (ví dụ:{" "}
+                  <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                    &lt;div&gt;
+                  </code>
+                  ,{" "}
+                  <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                    &lt;p&gt;
+                  </code>
+                  ).
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">
+                    inline
+                  </code>
+                  : Phần tử không tạo dòng mới, chỉ chiếm không gian vừa đủ cho
+                  nội dung (ví dụ:{" "}
+                  <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                    &lt;span&gt;
+                  </code>
+                  ,
+                  <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                    &lt;a&gt;
+                  </code>
+                  ).
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">
+                    inline-block
+                  </code>
+                  : Kết hợp giữa inline và block, phần tử có thể chỉnh sửa kích
+                  thước nhưng không tạo dòng mới.
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">none</code>
+                  : Ẩn phần tử khỏi trang, không chiếm không gian.
+                </li>
+              </ul>
+            </Card>
+          </div>
+        </div>
+      </section>
+      <section className="mb-12">
+        <div className="py-10 px-4">
+          <div className="max-w-7xl mx-auto" data-aos="fade-up">
+            <Card hoverable className="shadow-lg">
+              <Title level={2} className="text-left">
+                Khi nào sử dụng block, inline, inline-block và none?
+              </Title>
+              <ul className="list-inside list-disc text-lg">
+                <li>
+                  <code class="border border-gray-300 text-blue-600">
+                    block
+                  </code>
+                  : Dùng khi cần phần tử hiển thị ở dòng mới, bao phủ toàn bộ
+                  chiều rộng (ví dụ:{" "}
+                  <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                    &lt;section&gt;
+                  </code>
+                  ).
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">
+                    inline
+                  </code>
+                  : Dùng cho phần tử nằm trong văn bản, ví dụ: link hoặc từ khoá
+                  trong đoạn văn.
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">
+                    inline-block
+                  </code>
+                  : Khi muốn các phần tử nằm cùng dòng nhưng vẫn có khả năng
+                  chỉnh sửa kích thước (ví dụ:{" "}
+                  <code class="bg-gray-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                    &lt;button&gt;
+                  </code>
+                  ).
+                </li>
+                <li>
+                  <code class="border border-gray-300 text-blue-600">none</code>
+                  : Dùng khi cần ẩn phần tử tạm thời, chẳng hạn để tạo hiệu ứng
+                  hoặc tùy biến giao diện theo điều kiện.
+                </li>
+              </ul>
             </Card>
           </div>
         </div>
